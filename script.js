acac858dda42b7b21ace8a0cc4e4ce517ed4232d
Annaliisa Tongpattanakul <annaliisa@Annaliisas-MacBook-Pro.local>
2019-12-19 12:39:19 -0800

    allowed user to select character type, added notes, cleaned up code for better readability

--- script.js
+++ script.js
@@ -1,42 +1,59 @@
 //not exactly D.R.Y., but it's FUNCTIONAL!!!!!
 //password generator function
 function passGen() { 
-let userInput= prompt('Select a minimum password length between 8 and 128: ');
-if(userInput>=8 && userInput<=128 && userInput !== NaN) {
-    let low = 'abcdefghijklmnopqrstuvwxyz'; 
-    let up = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    let numeric = '0123456789';
-    let spec = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
-    let password = '';
-    //since there is a requirement that the user have one of each character type, 
-    //I designed the function to automatically generate one of each minimum, 
-    //You have to have all in order to meet the minimum total character requirement of 8.
+    let userInput= prompt('Select a minimum password length between 8 and 128: ');
+     //This ensures that the user selects a valid password length that is a number
+    if(userInput>=8 && userInput<=128 && userInput !== NaN) {
+        let low = 'abcdefghijklmnopqrstuvwxyz'; 
+        let up = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        let numeric = '0123456789';
+        let spec = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
+        let password = '';
+        //This is where the user is prompted for character types they would like their password to contain
+        let lowReq = confirm('Would you like your password to contain lower case letters?');
+        let upReq = confirm('Would you like your password to contain upper case letters?');
+        let numReq = confirm('Would you like your password to contain numbers?');
+        let specReq = confirm('Would you like your password to contain special characters?');
+
+        //This statement tells user they must have at least one type, the rest of the function
+        //will not run until at least one of the types returns true.
+        if(lowReq === false && upReq === false && numReq === false && specReq === false) {
+            alert("Your new password must contain at least one character type. Please try again.")
+        } else {
+
         while(password.length < userInput) {
-            if(password.length < userInput) {
+            //This will run if user selected ok for lower letters at prompt and password length has not been reached
+            if(password.length < userInput && lowReq === true) { 
                 let l = low.charAt(Math.floor(Math.random() * (low.length)));
                 password += l;
             }
-            if(password.length < userInput) {
+            //This will run if user selected ok for upper letters at the prompt and password length has not been reached
+            if(password.length < userInput && upReq === true) { 
                 let u = up.charAt(Math.floor(Math.random() * (up.length)));
                 password += u;  
             }
-            if(password.length < userInput) {
+            //This will run if user selected ok for numbers at the prompt and password length has not been reached
+            if(password.length < userInput && numReq === true) {
                 let n = numeric.charAt(Math.floor(Math.random() * (numeric.length)));
                 password += n;
             }
-            if(password.length < userInput) {
+            //This will run if user selected ok for special characters at the prompt and password length has not been reached
+            if(password.length < userInput && specReq === true) {
                 let s = spec.charAt(Math.floor(Math.random() * (spec.length)));
                 password += s;
             }
         }
+    }
         //this makes the chosen password characters appear in a more random order.
         password = password.split('').sort(function(){return 0.5-Math.random()}).join('');
         console.log(password);
         console.log(password.length);
         return password;
-} else {
+    } else {
         alert('That was not a valid input! Please try again.');
-    }}
+    }
+}   
+
 
 //added event listener to allow click of generate button to trigger the genPass function
 document.getElementById('generate').addEventListener('click', function () {
